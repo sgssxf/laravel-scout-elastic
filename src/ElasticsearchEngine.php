8c938753d1aeedee7a53de7c09e4374d76ce7972
srt <sgssxf@126.com>
2018-10-12 15:12:36 +0800

    解决数字不能搜索的问题

--- src/ElasticsearchEngine.php
+++ src/ElasticsearchEngine.php
@@ -138,7 +138,7 @@ class ElasticsearchEngine extends Engine
             'body' => [
                 'query' => [
                     'bool' => [
-                        'must' => [['query_string' => [ 'query' => "*{$builder->query}*"]]]
+                        'must' => [['query_string' => [ 'query' => "{$builder->query}"]]]
                     ]
                 ]
             ]
